e7180cdecfe7fa99e2bd2c15489d84da915a7b26
Shreyas Agarwal <adityarola@capx.fi>
2022-01-16 18:27:54 +0530

    Changes For DEMO

---
+++ src/utils/checkExistingProject.js
@@ -0,0 +1,71 @@
+import { ApolloClient, InMemoryCache, gql, cache } from "@apollo/client";
+
+import {
+  GRAPHAPIURL_MASTER_ETHEREUM,
+  GRAPHAPIURL_MASTER_BSC,
+  GRAPHAPIURL_MASTER_MATIC,
+} from "../constants/config";
+
+export const checkExistingProject = async (address, chainId, metamaskAccount) => {
+  let description = "";
+  let projectExistingData = [];
+  let data = {
+    name: "",
+    description: null,
+    exists: false,
+  };
+  const masterURL =
+    chainId === 4
+      ? GRAPHAPIURL_MASTER_ETHEREUM
+      : chainId === 97
+      ? GRAPHAPIURL_MASTER_BSC
+      : chainId === 80001
+      ? GRAPHAPIURL_MASTER_MATIC
+      : null;
+  const client = new ApolloClient({
+    uri: masterURL,
+    cache: new InMemoryCache(),
+  });
+  let projectID = `${metamaskAccount}-LOCK-${address}`;
+  const projectExistQuery = `query{
+    projects
+    
+    (where:{projectTokenAddress_contains : "${address}"})
+    {
+      projectOwnerAddress
+      projectName
+      projectTokenAddress
+      projectTokenTicker
+      projectDocHash
+    }
+  }`;
+  try {
+    projectExistingData = await client.query({
+      query: gql(projectExistQuery),
+      fetchPolicy: "network-only",
+    });
+    projectExistingData = projectExistingData?.data;
+    console.log(
+      projectExistingData?.projects[0]?.projectOwnerAddress,
+      "ped.po"
+    );
+    console.log(metamaskAccount, "met");
+
+    if (projectExistingData?.projects) {
+      const res = await fetch(
+        `https://milliondollarhomepage.mypinata.cloud/ipfs/${projectExistingData.projects[0].projectDocHash}`
+      );
+      const desc = await res.json();
+      description = desc.description;
+      console.log("description", description);
+      data = { name: projectExistingData.projects[0].projectName, description, exists:true };
+      return data;
+    }
+    else{
+        return data;
+    }
+  } catch (error) {
+    console.log(error);
+    return data;
+  }
+};

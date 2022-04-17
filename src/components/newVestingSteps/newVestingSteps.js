179e3868bcb08a76b2726b7312fdd2a3d4047a00
NachiketKotalwar <nachiket.k@capx.global>
2022-04-18 03:25:09 +0530

    Added assets
    Added assets to new temp pages

--- src/components/newVestingSteps/newVestingSteps.js
+++ src/components/newVestingSteps/newVestingSteps.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import DownloadIcon from '../../assets/download.png'
+import NextIcon from '../../assets/next-black.svg'
 import './newVestingSteps.scss'
 
 const newLanding = () => {
@@ -8,7 +10,7 @@ const newLanding = () => {
     <div className="newVestingScreen h-screen flex bg-dark-400">
       <Header hiddenNav />
       <div className="maincontainer flex gap-x-14 justify-center items-strech m-auto mt-auto">
-        <div className="herocontainer px-8 py-12 rounded-3xl bg-opacity-70 text-white relative w-37v flex flex-col items-start">
+        <div className="herocontainer px-9 py-12 rounded-3xl bg-opacity-70 text-white relative w-30v flex flex-col items-start">
           <div className="title text-5xl font-bold leading-lh-54 tracking-tight mt-2 w-10/12 text-left">
             {"Vest Tokens in 3 simple steps"}
           </div>
@@ -25,12 +27,14 @@ const newLanding = () => {
           </div>
         </div>
 
-        <div className="side-container bg-opacity-70 text-white relative w-37v flex flex-col justify-between">
+        <div className="side-container bg-opacity-70 text-white relative w-30v flex flex-col justify-between">
           <div className="download-template py-10 justify-self-start flex flex-col rounded-3xl items-center">
-            <div className="title text-5xl text-center font-bold leading-lh-54 tracking-tight">
-              .<br></br>.<br></br>.
+            <div className="xldiv rounded-2xl py-6 text-center font-semi-bold text-caption-2 leading-caption-2">
+              <img src={DownloadIcon} alt="Download Icon" className='inline-block mx-auto'/>
+              <div className='w-3/4 mx-auto mt-2'>Capx Vesting Sheet Template.xlsx</div>
             </div>
-            <div className="text-subheading leading-subheading mt-2 text-center font-bold">
+
+            <div className="text-subheading leading-subheading mt-6 text-center font-bold">
               Download Vesting Sheet Template
             </div>
 
@@ -38,10 +42,11 @@ const newLanding = () => {
               To proceed further with an error-free vesting experience we
               recommend adding the token details in the following template.
             </div>
+            
           </div>
           <div className="side-button justify-self-end rounded-2xl justify-center items-center flex px-1 py-3 w-full cursor-pointer">
             <div className="button_text text-black text-subheading leading-text-subheading font-bold">
-              {"Begin Vesting"}
+              {"Begin Vesting"}<img src={NextIcon} alt="Next Icon" className='inline-block w-7 ml-3 mr-2'></img>
             </div>
           </div>
         </div>

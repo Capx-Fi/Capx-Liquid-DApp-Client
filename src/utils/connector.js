bc80a8ee2a51435d24cbd229bc3ae8b62ec7fbaf
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-04-27 13:58:10 +0530

    added walletconnect functionality

--- src/utils/connector.js
+++ src/utils/connector.js
@@ -1,9 +1,15 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+
 export const injected = new InjectedConnector({
-  supportedChainIds: process.env.REACT_APP_SUPPORTED_CHAIN_IDS.replace(
-    /['"]+/g,
-    ""
-  )
-    .split(", ")
-    .map(Number),
+	supportedChainIds: process.env.REACT_APP_SUPPORTED_CHAIN_IDS.replace(
+		/['"]+/g,
+		""
+	)
+		.split(", ")
+		.map(Number),
+});
+
+export const walletconnect = new WalletConnectConnector({
+	qrcode: true,
 });

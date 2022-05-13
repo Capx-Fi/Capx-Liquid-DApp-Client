f070868c5ac0d4e7d916d8f0cf319c56b9e09252
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-05-13 17:55:04 +0530

    added breakpoint check

--- src/App.js
+++ src/App.js
@@ -9,35 +9,49 @@ import LoadingScreen from "./containers/LoadingScreen";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Landing from "./components/Landing/Landing";
 import ChooseWalletModal from "./components/Modal/ChooseWalletModal/ChooseWalletModal";
+import Header from "./components/Header/Header";
+import BreakPoint from "./containers/Breakpoint";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1500);
-  }, []);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 1500);
+	}, []);
 
-  return (
-    <>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/vesting" component={VestingScreen} />
-            <Route exact path="/investor" component={InvestorDashboardScreen} />
-            <Route
-              exact
-              path="/projectoverview"
-              component={ProjectOwnerDashboardScreen}
-            />
-            <Route exact path="/1" component={ChooseWalletModal} />
-            <Route path="*" component={PageNotFound} />
-          </Switch>
-        </Router>
-      )}
-    </>
-  );
+	return (
+		<>
+			{loading ? (
+				<LoadingScreen />
+			) : (
+				<Router>
+					<div className="screen:block hidden">
+						<Switch>
+							<Route exact path="/" component={Landing} />
+							<Route exact path="/vesting" component={VestingScreen} />
+							<Route
+								exact
+								path="/investor"
+								component={InvestorDashboardScreen}
+							/>
+							<Route
+								exact
+								path="/projectoverview"
+								component={ProjectOwnerDashboardScreen}
+							/>
+							<Route exact path="/1" component={ChooseWalletModal} />
+							<Route path="*" component={PageNotFound} />
+						</Switch>
+					</div>
+					<div className="screen:hidden m-auto block align-middle justify-center items-center overflow-x-hidden">
+						<Header hiddenNav />
+						<BreakPoint />
+						<Footer />
+					</div>
+				</Router>
+			)}
+		</>
+	);
 }
 
 export default App;

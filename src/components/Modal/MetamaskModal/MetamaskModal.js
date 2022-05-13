63f926a2c55d30c8bdc0c8c3cbfd215f223361f9
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-05-13 20:46:21 +0530

    added a breakpoint fix and stabilised wallet connection

--- src/components/Modal/MetamaskModal/MetamaskModal.js
+++ src/components/Modal/MetamaskModal/MetamaskModal.js
@@ -10,50 +10,73 @@ import { useSnackbar } from "notistack";
 import { useTranslation } from "react-i18next";
 import "../../../translations/i18n";
 import { CHAIN_NAMES } from "../../../constants/config";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function MetamaskModal({setModalMode}) {
+function MetamaskModal({ setModalMode }) {
 	const { t } = useTranslation();
-	
+	const { active, account, library, connector, activate, error } =
+		useWeb3React();
+	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+	const unsupportedChainIdError =
+		error && error instanceof UnsupportedChainIdError;
+
+	async function connect() {
+		try {
+			await activate(injected);
+			if (unsupportedChainIdError) {
+				enqueueSnackbar(`Please connect to the ${CHAIN_NAMES} Mainnet Chain.`, {
+					variant: "error",
+				});
+			}
+		} catch (ex) {
+			if (error instanceof UnsupportedChainIdError) {
+				enqueueSnackbar(`Please connect to the ${CHAIN_NAMES} Mainnet Chain.`, {
+					variant: "error",
+				});
+			}
+			alert(ex);
+		}
+	}
+
 	return (
-    <article className="metamaskmodalscreen">
-      <Header hiddenNav />
-      <section className="metamaskmodalscreen_maincontainer">
-        <div className="metamaskmodalscreen_maincontainer_herocontainer">
-          <div className="metamaskmodalscreen_maincontainer_herocontainer_title">
-            {t("please_connect_metamask")}
-            <br /> {t("please_connect_metamask_2")}
-          </div>
-          {/* <div className="metamaskmodalscreen_maincontainer_herocontainer_title">
+		<article className="metamaskmodalscreen">
+			<Header hiddenNav />
+			<section className="metamaskmodalscreen_maincontainer">
+				<div className="metamaskmodalscreen_maincontainer_herocontainer">
+					<div className="metamaskmodalscreen_maincontainer_herocontainer_title">
+						{t("please_connect_metamask")}
+						<br /> {t("please_connect_metamask_2")}
+					</div>
+					{/* <div className="metamaskmodalscreen_maincontainer_herocontainer_title">
             {t("please_connect_metamask_2")}
           </div> */}
-          
-            <div
-              className="metamaskmodalscreen_maincontainer_herocontainer_button"
-              onClick={() => {
-                setModalMode(1);
-              }}
-            >
-              <div className="metamaskmodalscreen_maincontainer_herocontainer_button_text">
-                {"Connect Wallet"}
-              </div>
-              <img
-                className="metamaskmodalscreen_maincontainer_herocontainer_button_icon"
-                src={NextIcon}
-				alt="Next Icon"
-              />
-            </div>
-          
-          <img
-            className="metamaskmodalscreen_maincontainer_herocontainer_firefoxillustration"
-            src={FirefoxIllustration}
-            alt="ETH Illustration"
-          />
-        </div>
-      </section>
-      <Footer />
-    </article>
-  );
+
+					<div
+						className="metamaskmodalscreen_maincontainer_herocontainer_button"
+						onClick={() => {
+							connect();
+						}}
+					>
+						<div className="metamaskmodalscreen_maincontainer_herocontainer_button_text">
+							{"Connect Metamask"}
+						</div>
+						<img
+							className="metamaskmodalscreen_maincontainer_herocontainer_button_icon"
+							src={NextIcon}
+							alt="Next Icon"
+						/>
+					</div>
+
+					<img
+						className="metamaskmodalscreen_maincontainer_herocontainer_firefoxillustration"
+						src={FirefoxIllustration}
+						alt="ETH Illustration"
+					/>
+				</div>
+			</section>
+			<Footer />
+		</article>
+	);
 }
 
 export default MetamaskModal;

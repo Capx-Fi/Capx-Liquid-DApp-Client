bc8810dcb67293e6a59c1df1f16e30df0c468f2c
NachiketKotalwar <nachiket.k@capx.global>
2022-06-24 14:59:29 +0530

    Added copy icon and responsive rendering for addresses

--- src/containers/ProjectOwnerDashboard/ProjectDetailsContainer.js
+++ src/containers/ProjectOwnerDashboard/ProjectDetailsContainer.js
@@ -5,6 +5,10 @@ import { useEffect, useState } from "react";
 import ProjectDetailsLoading from "./ProjectDetailsLoading";
 import { useWeb3React } from "@web3-react/core";
 import Web3 from "web3";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import CopyIcon from "../../assets/copy-icon.svg";
+import { Tooltip } from "@material-ui/core";
+import useWindowSize from "../../utils/windowSize";
 
 const currentDate = new Date();
 let datetime = currentDate.toLocaleString("en-US");
@@ -18,8 +22,30 @@ function ProjectDetailsContainer({ projectOverviewData, projectDisplayID }) {
 		displayProjectDetails();
 	}, [projectDisplayID, projectOverviewData]);
 
+	const [contractAddressCopied, setContractAddressCopied] = useState(false);
+	const [creatorAddressCopied, setCreatorAddressCopied] = useState(false);
+	const windowWidth = useWindowSize().width;
+
+
 	const { chainId } = useWeb3React();
 	const [project, setProject] = useState(null);
+
+	useEffect(() => {
+    if (contractAddressCopied) {
+      setTimeout(() => {
+        setContractAddressCopied(false);
+      }, 2000);
+    }
+	}, [contractAddressCopied]);
+
+	useEffect(() => {
+    if (creatorAddressCopied) {
+      setTimeout(() => {
+        setCreatorAddressCopied(false);
+      }, 2000);
+    }
+  }, [creatorAddressCopied]);
+	
 	const displayProjectDetails = async () => {
 		if (projectOverviewData.length > 0) {
 			let description = "N/A";
@@ -44,59 +70,113 @@ function ProjectDetailsContainer({ projectOverviewData, projectDisplayID }) {
 		}
 	};
 	return project ? (
-		<section className="projectdetailscontainer">
-			<div className="projectdetailscontainer_title">DETAILS</div>
-			<hr className="border-greyborder opacity-50 -mx-6 h-2"></hr>
-			<div className="projectdetailscontainer_innercontainer">
-				<div className="flex justify-between">
-					<div className="projectdetailscontainer_innercontainer_detailbox">
-						<div className="projectdetailscontainer_innercontainer_detailbox_key">
-							PROJECT NAME
-						</div>
-						<div className="projectdetailscontainer_innercontainer_detailbox_value">
-							{project?.projectName}
-						</div>
-					</div>
-					<div className="projectdetailscontainer_innercontainer_detailbox">
-						<div className="projectdetailscontainer_innercontainer_detailbox_key">
-							TOKEN TICKER
-						</div>
-						<div className="projectdetailscontainer_innercontainer_detailbox_value">
-							{project?.tokenTicker}
-						</div>
-					</div>
-				</div>
-				<div className="projectdetailscontainer_innercontainer_detailbox">
-					<div className="projectdetailscontainer_innercontainer_detailbox_key">
-						CONTRACT ADDRESS
-					</div>
-					<div className="projectdetailscontainer_innercontainer_detailbox_value">
-						{project?.contractAddress}
-					</div>
-				</div>
-				<div className="projectdetailscontainer_innercontainer_detailbox">
-					<div className="projectdetailscontainer_innercontainer_detailbox_key">
-						CREATOR ADDRESS
-					</div>
-					<div className="projectdetailscontainer_innercontainer_detailbox_value">
-						{project?.projectOwnerAddress}
-					</div>
-				</div>
-				<div className="projectdetailscontainer_innercontainer_detailbox">
-					<div className="projectdetailscontainer_innercontainer_detailbox_key">
-						PROJECT DESCRIPTION
-					</div>
-					<div
-						className="projectdetailscontainer_innercontainer_detailbox_value projectdetailscontainer_innercontainer_detailbox_value_description"
-						dangerouslySetInnerHTML={{ __html: project?.projectDescription }}
-					></div>
-				</div>
-				<hr className="border-greyborder opacity-50 -mx-6 mt-7 h-2"></hr>
-			</div>
-		</section>
-	) : (
-		<ProjectDetailsLoading />
-	);
+    <section className="projectdetailscontainer">
+      <div className="projectdetailscontainer_title">DETAILS</div>
+      <hr className="border-greyborder opacity-50 -mx-6 h-2"></hr>
+      <div className="projectdetailscontainer_innercontainer">
+        <div className="flex justify-between">
+          <div className="projectdetailscontainer_innercontainer_detailbox">
+            <div className="projectdetailscontainer_innercontainer_detailbox_key">
+              PROJECT NAME
+            </div>
+            <div className="projectdetailscontainer_innercontainer_detailbox_value">
+              {project?.projectName}
+            </div>
+          </div>
+          <div className="projectdetailscontainer_innercontainer_detailbox">
+            <div className="projectdetailscontainer_innercontainer_detailbox_key">
+              TOKEN TICKER
+            </div>
+            <div className="projectdetailscontainer_innercontainer_detailbox_value">
+              {project?.tokenTicker}
+            </div>
+          </div>
+        </div>
+        <div className="projectdetailscontainer_innercontainer_detailbox">
+          <div className="projectdetailscontainer_innercontainer_detailbox_key">
+            CONTRACT ADDRESS
+          </div>
+          <div className="projectdetailscontainer_innercontainer_detailbox_value">
+            {windowWidth >= 1700
+              ? project?.contractAddress
+              : `${project?.contractAddress?.substr(
+                  0,
+                  6
+                )}...${project?.contractAddress?.substr(-4)}`}
+            <CopyToClipboard
+              text={project?.contractAddress}
+              onCopy={() => setContractAddressCopied(true)}
+            >
+              <button className="inline-block">
+                <Tooltip
+                  title={
+                    <span className="text-caption-2 block p-1 font-medium">
+                      Copied
+                    </span>
+                  }
+                  open={contractAddressCopied}
+                  arrow
+                >
+                  <img
+                    src={CopyIcon}
+                    className="w-4 ml-2 -mt-0.5"
+                    alt="Copy Icon"
+                  />
+                </Tooltip>
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <div className="projectdetailscontainer_innercontainer_detailbox">
+          <div className="projectdetailscontainer_innercontainer_detailbox_key">
+            CREATOR ADDRESS
+          </div>
+          <div className="projectdetailscontainer_innercontainer_detailbox_value">
+            {windowWidth >= 1700
+              ? project?.projectOwnerAddress
+              : `${project?.projectOwnerAddress?.substr(
+                  0,
+                  6
+                )}...${project?.projectOwnerAddress?.substr(-4)}`}
+            <CopyToClipboard
+              text={project?.projectOwnerAddress}
+              onCopy={() => setCreatorAddressCopied(true)}
+            >
+              <button className="inline-block">
+                <Tooltip
+                  title={
+                    <span className="text-caption-2 block p-1 font-medium">
+                      Copied
+                    </span>
+                  }
+                  open={creatorAddressCopied}
+                  arrow
+                >
+                  <img
+                    src={CopyIcon}
+                    className="w-4 ml-2 -mt-0.5"
+                    alt="Copy Icon"
+                  />
+                </Tooltip>
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <div className="projectdetailscontainer_innercontainer_detailbox">
+          <div className="projectdetailscontainer_innercontainer_detailbox_key">
+            PROJECT DESCRIPTION
+          </div>
+          <div
+            className="projectdetailscontainer_innercontainer_detailbox_value projectdetailscontainer_innercontainer_detailbox_value_description"
+            dangerouslySetInnerHTML={{ __html: project?.projectDescription }}
+          ></div>
+        </div>
+        <hr className="border-greyborder opacity-50 -mx-6 mt-7 h-2"></hr>
+      </div>
+    </section>
+  ) : (
+    <ProjectDetailsLoading />
+  );
 }
 
 export default ProjectDetailsContainer;
